6d0a39d24e512d2c35cda30b53ab5955730b4529
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-09-14 23:21:13 +0300

    Update react-native.config.js

--- packages/app/react-native.config.js
+++ packages/app/react-native.config.js
@@ -1,15 +1,24 @@
+const isCI = process.env.CI === 'true';
+
 module.exports = {
+  // Path to the react-native package
   reactNativePath: '../../node_modules/react-native',
+
+  // Dependency-specific configurations
   dependencies: {
+    // Disable Flipper in CI environment
     'react-native-flipper': {
-      // disable flipper in CI environment
-      platforms: process.env.CI ? { ios: null, android: null } : {},
+      platforms: isCI ? { ios: null, android: null } : {},
     },
+
+    // Disable Google Sign-In for iOS
     '@react-native-google-signin/google-signin': {
       platforms: {
         ios: null,
       },
     },
+
+    // Disable react-native-v8 for iOS
     'react-native-v8': {
       platforms: {
         ios: null,
