52fbba04f98512120fc32f049dee7f4ced8609ca
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-09-17 23:03:33 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
-// https://jestjs.io/docs/configuration
 const { defaults } = require('jest-config');
 const util = require('node:util');
 const exec = util.promisify(require('node:child_process').exec);
@@ -6,17 +5,15 @@ const exec = util.promisify(require('node:child_process').exec);
 module.exports = async () => {
   const { stdout } = await exec('yarn config get cacheFolder');
   const cacheDirectory = stdout.trim().replace('\n', '');
+
   return {
-    // ts-jest, react-native, jest-expo
     preset: 'jest-expo',
     coverageProvider: 'v8',
-    cacheDirectory: `${cacheDirectory}/.app-mono-jest-cache`,
+    cacheDirectory: `${cacheDirectory}/.baron-mono-jest-cache`,
     setupFilesAfterEnv: [
       './jest-setup.js',
       './node_modules/react-native-gesture-handler/jestSetup.js',
     ],
-    // buffer type incorrect if use [jsdom] https://github.com/facebook/jest/issues/4422
-    // jest-environment-node, node, jsdom
     testEnvironment: 'jest-environment-node',
     verbose: true,
     moduleFileExtensions: [
@@ -30,7 +27,6 @@ module.exports = async () => {
         '<rootDir>/__mocks__/fileMock.js',
       '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
     },
-    // TODO unify with transpile modules
     transformIgnorePatterns: ['nodo_modules/react-native-reanimated'],
     reporters: [
       'default',
