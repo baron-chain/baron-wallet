eaa805ba99b3835983acb47a54bf68332747fe34
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-09-19 22:36:34 +0300

    Update babel.config.js

--- packages/web/babel.config.js
+++ packages/web/babel.config.js
@@ -3,7 +3,7 @@ const babelTools = require('../../development/babelTools');
 module.exports = babelTools.normalizeConfig({
   platform: babelTools.developmentConsts.platforms.web,
   config: {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo1'],
     plugins: ['react-native-reanimated/plugin'],
   },
 });
