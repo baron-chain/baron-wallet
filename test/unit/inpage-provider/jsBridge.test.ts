384916d6f4a0722237e549cda4806ca063d5e9e7
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-09-19 22:32:14 +0300

    Update jsBridge.test.ts

--- test/unit/inpage-provider/jsBridge.test.ts
+++ test/unit/inpage-provider/jsBridge.test.ts
@@ -1,33 +1,29 @@
 // @ts-nocheck
-import { JsBridgeSimple } from '@onekeyfe/cross-inpage-provider-core';
-
-import type { IJsBridgeMessagePayload } from '@onekeyfe/cross-inpage-provider-types';
+import { JsBridgeSimple } from '@baronfe/cross-inpage-provider-core';
+import type { IJsBridgeMessagePayload } from '@baronfe/cross-inpage-provider-types';
 
 describe('CrossInpageProvider Tests', () => {
   it('two bridge communication', async () => {
     let currentChainId = '0x3';
-    // TODO sendAsString=false
+    
     const host = new JsBridgeSimple({
-      timeout: 0, // TODO remove, fix in JsBridgeSimple: this.callbacksExpireTimeout=0
+      timeout: 0,
       sendAsString: false,
       receiveHandler(event: IJsBridgeMessagePayload) {
-        // throw new Error('hhhhhh');
         const { method } = event.data;
         if (method === 'eth_chainId') {
           return currentChainId;
-          // return '0x22';
         }
       },
     });
+    
     const inpage = new JsBridgeSimple({
-      timeout: 0, // TODO remove, fix in JsBridgeSimple: this.callbacksExpireTimeout=0
+      timeout: 0,
       sendAsString: false,
       receiveHandler(event: IJsBridgeMessagePayload) {
         const { method, params } = event.data;
         if (method === 'metamask_chainChanged') {
           currentChainId = params.chainId;
-          // TODO should response here force resolve Promise call
-          // inpage.response(event.id, '');
         }
       },
     });
@@ -35,12 +31,10 @@ describe('CrossInpageProvider Tests', () => {
     host.setRemote(inpage);
     inpage.setRemote(host);
 
-    // TODO async event handler?
     host.on('error', (error) => {
       console.log(error);
     });
 
-    // ----------------------------------------------
     const chainId = await inpage.request({
       scope: 'ethereum',
       data: { method: 'eth_chainId' },
