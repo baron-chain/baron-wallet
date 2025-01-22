69a3781624dd71531a360f5f1ccba744d51f5942
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2025-01-23 00:41:24 +0200

    Update batchTransfer.ts

--- packages/engine/src/types/batchTransfer.ts
+++ packages/engine/src/types/batchTransfer.ts
@@ -1,23 +1,34 @@
-export enum BatchTransferMethods {
-  disperseNFT = 'disperseNFT(address,address[],uint256[],uint256[])',
-  disperseEther = 'disperseEther(address[],uint256[])',
-  disperseToken = 'disperseToken(address,address[],uint256[])',
-  disperseTokenSimple = 'disperseTokenSimple(address,address[],uint256[])',
-  disperseEtherSameValue = 'disperseEtherSameValue(address[],uint256)',
-  disperseTokenSameValue = 'disperseTokenSameValue(address,address[],uint256)',
-}
+package types
 
-export enum BatchTransferSelectors {
-  disperseNFT = '0x39039af6',
-  disperseEther = '0xe63d38ed',
-  disperseToken = '0xc73a2d60',
-  disperseTokenSimple = '0x51ba162c',
-  disperseEtherSameValue = '0xc263a3e4',
-  disperseTokenSameValue = '0x17546c6c',
-}
+// BatchTransferMethods defines the method signatures for batch transfers
+type BatchTransferMethods string
 
-export enum BulkTypeEnum {
-  OneToMany = 'OneToMany',
-  ManyToMany = 'ManyToMany',
-  ManyToOne = 'ManyToOne',
-}
+const (
+    DisperseBatchNFT              BatchTransferMethods = "disperseNFT(address,address[],uint256[],uint256[])"
+    DisperseBatchEther            BatchTransferMethods = "disperseEther(address[],uint256[])"
+    DisperseBatchToken            BatchTransferMethods = "disperseToken(address,address[],uint256[])"
+    DisperseBatchTokenSimple      BatchTransferMethods = "disperseTokenSimple(address,address[],uint256[])"
+    DisperseBatchEtherSameValue   BatchTransferMethods = "disperseEtherSameValue(address[],uint256)"
+    DisperseBatchTokenSameValue   BatchTransferMethods = "disperseTokenSameValue(address,address[],uint256)"
+)
+
+// BatchTransferSelectors defines the method selectors for batch transfers
+type BatchTransferSelectors string
+
+const (
+    SelectorDisperseBatchNFT              BatchTransferSelectors = "0x39039af6"
+    SelectorDisperseBatchEther            BatchTransferSelectors = "0xe63d38ed"
+    SelectorDisperseBatchToken            BatchTransferSelectors = "0xc73a2d60"
+    SelectorDisperseBatchTokenSimple      BatchTransferSelectors = "0x51ba162c"
+    SelectorDisperseBatchEtherSameValue   BatchTransferSelectors = "0xc263a3e4"
+    SelectorDisperseBatchTokenSameValue   BatchTransferSelectors = "0x17546c6c"
+)
+
+// BulkTransferType defines the type of bulk transfer
+type BulkTransferType string
+
+const (
+    OneToMany    BulkTransferType = "OneToMany"
+    ManyToMany   BulkTransferType = "ManyToMany"
+    ManyToOne    BulkTransferType = "ManyToOne"
+)
